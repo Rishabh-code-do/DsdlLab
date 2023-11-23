a0fa81f6d5edcfa39c57d158ee8566f0530bc580
RISHABH JAISWAL <85050354+Rishabh-code-do@users.noreply.github.com>
2023-11-23 16:00:34 +0530

    Update tenth

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
   return (
     <>
      <div className='hero'>
-       <p className='font1'>"Unleasing Power Where Supercomputing Transcends Boundaries and Innovates."</p>
+       <p className='font1'>"Unleashing Power Where Supercomputing Transcends Boundaries and Innovates."</p>
        <p className='font2'>☆ Welcome to DSDL Lab</p>
        <p className='font3'>DSDL Club Offers cutting-edge supercomputer access for brilliant minds.<br></br>Open to all, including alumini and external researchers.Empowering innovation and collabration beyond boundaries.<br></br>Join Us for limited possibilites.</p>
        <div className='btn'>
