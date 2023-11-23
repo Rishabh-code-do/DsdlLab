08f6e5a60bde96ea1458bf883856a88179d312ba
RISHABH JAISWAL <85050354+Rishabh-code-do@users.noreply.github.com>
2023-11-23 15:04:58 +0530

    Update seventh

--- src/Components/Founder.jsx
+++ src/Components/Founder.jsx
@@ -11,8 +11,8 @@ function Founder() {
         <div className='cont1'>
         <img src={GP} alt="GP" className='img1'/>
         <p className='para'>
-        Mr. Gaurav Parashar<br></br>
-        Assistant Professor<br></br> Department of Computer Science & Engineering<br></br>
+        Dr. Vineet Sharma<br></br>
+        Head of Department(HOD)<br></br> Department of Computer Science & Engineering<br></br>
         <span className='email'>Email Id :- gaurav.parashar@kiet.edu</span>
         </p>
         </div>
